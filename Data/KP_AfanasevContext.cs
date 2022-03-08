a24801e8d52b5b1881d74ffd46bc7c8ca745c008
vasterman666 <71226546+vasterman666@users.noreply.github.com>
2022-03-08 20:43:03 +0300

    Initial commit

---
+++ Data/KP_AfanasevContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace KP_Afanasev.Data
+{
+    public class KP_AfanasevContext : DbContext
+    {
+        public KP_AfanasevContext (DbContextOptions<KP_AfanasevContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KP_Afanasev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
